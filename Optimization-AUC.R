36b1a66f709524d2e35ceeff6c0e42cfb3445fa8
luca <luca@mon_ordinateur>
2020-11-15 13:27:44 +0100

    Version commentée

--- Optimization-AUC.R
+++ Optimization-AUC.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 library(pROC)
 
+# Données
+
 bw <- MASS::birthwt %>% 
   select(low, smoke, ui, ht)
 
-
+# Fonction permettant de calculer l'aire sous la courbe pour un modèle de régression logistique donné
 
 aire <- function(coef) {
   beta_0 <- coef[1]
@@ -21,7 +23,7 @@ aire <- function(coef) {
     pull(c.index)
 }
 
-
+# Optimisation de cette fonction (maximisation de l'aire sous la courbe)
 
 opt <- optim(
   par = c(b_0 = 0, b_smoke = 0, b_ht = 0, b_ui = 0),
@@ -31,6 +33,12 @@ opt <- optim(
 
 opt
 names(opt)
+
+# Paramètres estimés qui maximisent l'aire sous la courbe
+
 opt$par
+
+# Aire sous la courbe correspondant à ce modèle estimé (AUC = 0.6632)
+
 aire(c(opt$par[1], opt$par[2], opt$par[3], opt$par[4]))
 
